2ad387d2b8500b745272f41d6fa20ec1e5d16f1e
Osmel Lavalier <o.lavalier@gmail.com>
2020-11-10 16:03:49 +0100

    first version 0.1
    Progress in work laboratory 2.

---
+++ Laboratory 2/Laboratory 2/shape.h
@@ -0,0 +1,19 @@
+//
+//  shape.h
+//  Laboratory 2
+//
+//  Created by Osmel Lavalier Martin on 2020-11-10.
+//
+
+#ifndef shape_h
+#define shape_h
+
+#include<bits/stdc++.h>
+
+
+class shape
+{
+   
+};
+
+#endif /* shape_h */
